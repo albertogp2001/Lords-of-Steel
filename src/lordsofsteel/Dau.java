ba31d9607318059e500051aa4e0fec7a3b3088b2
Alberto Gomez <gomez.perez.alberto@alumnat.copernic.cat>
2023-05-08 13:10:23 +0200

    Creamos interfaz ordre

---
+++ src/lordsofsteel/Dau.java
@@ -0,0 +1,17 @@
+package lordsofsteel;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Alberto
+ */
+public class Dau {
+
+    final int MAX = 25;
+
+   
+}
